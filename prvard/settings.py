5307af8606c7dcaa3cc664dae48042418699ecc5
Dev.Abdelrman <prro.code@gmail.com>
2025-08-04 20:20:43 +0300

    Update settings.py

--- prvard/settings.py
+++ prvard/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+)3j_k9c2oo)v^3ttz(j-a_wvn^c8w=!4)ysmnl5t#6ylg2gwz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["prvard.onrender.com"]
 
 
 # Application definition
